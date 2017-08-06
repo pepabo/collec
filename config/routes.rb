6a9bcb8c43192eb5d45d588c3ba8b512c6f0d7c3
takumakume <takuma.kume@gmail.com>
2017-08-06 15:15:39 +0900

    依頼一覧画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'messages/create'
+  get 'messages/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
