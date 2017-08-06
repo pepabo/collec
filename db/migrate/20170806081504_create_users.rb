1421c4187856fdf9499262be84612aa27af5a09f
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-07 00:10:22 +0900

    usersテーブルにSlackのユーザーIDを格納するカラムを追加

--- db/migrate/20170806081504_create_users.rb
+++ db/migrate/20170806081504_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :slack_id
       t.string :access_token
       t.string :user_name
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170806081504) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "slack_id"
     t.string "access_token"
     t.string "user_name"
     t.datetime "created_at", null: false
