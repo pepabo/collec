c37a41704afd8d387940108585a4ee83d3e5dd2e
takumakume <takuma.kume@gmail.com>
2017-08-07 23:34:40 +0900

    messages/indexを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   get 'messages/create'
   resources :sessions, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
