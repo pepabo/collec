61048a6b84d1df33a8766168566ad57ae204ed57
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-08 22:36:59 +0900

    message_buttonsテーブルのカラム名を調整
    * 選択したアクションの識別子用カラムはvalue -> nameに変更。SlackのMessage action fieldsとしてはどちらもほぼ同じ意味で、選択したボタンを識別するためにはどちらも使える。nameは必須、valueは任意という点から、APIに合わせてnameにする
    * ボタン名のカラムはname -> textに変更。APIのMessage action fieldsの項目名に合わせた。
    
    ref: https://api.slack.com/docs/interactive-message-field-guide#action_fields

--- db/migrate/20170807135658_create_message_buttons.rb
+++ db/migrate/20170807135658_create_message_buttons.rb
@@ -2,8 +2,8 @@ class CreateMessageButtons < ActiveRecord::Migration[5.1]
   def change
     create_table :message_buttons do |t|
       t.references :message, foreign_key: true
-      t.string :name, comment: 'ユーザーが入力したボタン名'
-      t.string :value, comment: 'ユーザーが選択したアクションを識別するための文字列'
+      t.string :name, comment: 'ユーザーが選択したアクションを識別するための文字列'
+      t.string :text, comment: 'ユーザーが入力したボタン名'
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20170807141535) do
 
   create_table "message_buttons", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "message_id"
-    t.string "name", comment: "ユーザーが入力したボタン名"
-    t.string "value", comment: "ユーザーが選択したアクションを識別するための文字列"
+    t.string "name", comment: "ユーザーが選択したアクションを識別するための文字列"
+    t.string "text", comment: "ユーザーが入力したボタン名"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_message_buttons_on_message_id"
