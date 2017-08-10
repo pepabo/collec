8ac3df3ef33be7becf9045ddfce42604f0d9b32e
Shinya Tsunematsu <s@tnmt.info>
2017-08-10 17:18:12 +0900

    FactoryGirlを使ってテストデータを作って件数をテスト

---
+++ spec/factories/message.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :message do
+    message 'hoge'
+    slack_user
+  end
+
+  factory :slack_user do
+    slack_user_id = 1
+
+    factory :slack_user_with_messages do
+      transient do
+        messages_count 10
+      end
+
+      after(:create) do |slack_user, evaluator|
+        create_list(:message, evaluator.messages_count, slack_user: slack_user)
+      end
+    end
+  end
+end
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe "Messages", type: :request do
   describe "GET /api/v1/messages" do
     before do
+      create(:slack_user_with_messages)
       get '/api/v1/messages', headers: { HTTP_ACCEPT: 'application/json', CONTENT_TYPE: 'application/json' }
     end
 
     it 'response 200' do
       expect(response).to be_success
       expect(response.status).to eq 200
+    end
+
+    it 'return all messages' do
+      result = JSON.parse(response.body)
+      expect(result.count).to eq 10
     end
   end
 end
