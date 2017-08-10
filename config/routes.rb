7cbb1864ea21bb5a25aa67795a8f386be7f8cc35
Shinya Tsunematsu <s@tnmt.info>
2017-08-10 15:50:20 +0900

    use the new Ruby 1.9 hash syntax.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :messages, :only => :index
+      resources :messages, only: :index
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
