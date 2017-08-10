f6d0daa194c8d872bdb1047a5f523e20b3fa8536
Shinya Tsunematsu <s@tnmt.info>
2017-08-10 15:58:38 +0900

    use rspec

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,5 @@
 # https://railsguides.jp/generators.html
 Rails.application.config.generators do |g|
-  g.test_framework :test_unit,
+  g.test_framework :rspec,
     fixture_replacement: :factory_girl
 end
