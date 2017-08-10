e0bc40ed4821e2b2f0749d30bddf7300ff793de6
Shinya Tsunematsu <s@tnmt.info>
2017-08-10 18:51:12 +0900

    access_tokenからslack_user_idを引いてくるところをいったん置いて決め打ち

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.where("slack_user_id = ?", 1)
     render json: @messages
   end
 end
