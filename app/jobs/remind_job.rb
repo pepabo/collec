fcd78c6e549a10922829d707609a7fad0b8fb923
takumakume <takuma.kume@gmail.com>
2017-08-12 23:23:44 +0900

    init remind job

---
+++ app/jobs/remind_job.rb
@@ -0,0 +1,7 @@
+class RemindJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/remind_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RemindJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
