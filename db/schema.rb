938baafbe487a1c24e93a6c74e676127ac8a8572
Shinya Tsunematsu <s@tnmt.info>
2017-08-12 17:51:59 +0900

    コメントは前のものが残るように

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20170812081211) do
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
     t.text "message", comment: "Request message"
-    t.datetime "due_at"
+    t.datetime "due_at", comment: "Due to answer the request"
     t.boolean "require_confirm", default: false, comment: "Flag to use confirmation dialog"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
