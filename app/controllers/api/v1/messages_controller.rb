bd3456a17ad06c5c697aa6ac6d90d954bb74df4b
Shinya Tsunematsu <s@tnmt.info>
2017-08-12 18:42:28 +0900

    renderの記述無しでもOKなので冗長な記載は消す

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::MessagesController < ApplicationController
   def index
     @messages = Message.where("user_id = ?", 1)
-    render 'index', format: 'json', handlers: 'jbuilder'
   end
 end
