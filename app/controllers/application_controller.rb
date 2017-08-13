b5efd9caa6ddd561124613f3534e4152a8f9fadb
pyama86 <pyama@pepabo.com>
2017-08-13 22:55:12 +0900

    deviseどーーーーーん

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    session[:user_return_to] = ENV['PATH_INFO']
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
