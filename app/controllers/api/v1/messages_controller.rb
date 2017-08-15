3a2abc7bd68d57d4ed26aad7a549a69f4288a9a4
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-15 16:02:25 +0900

    リレーションの中に値を挿入し、最後に一括登録をさせる

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -11,19 +11,13 @@ class Api::V1::MessagesController < ApplicationController
     Message.transaction do
       message = Message.new(message_attributes)
       message.user_id = 1 # TODO: Pass the user id parameter from payload user id in JWT.
-      message.save
-
       message_buttons_attributes[:message_buttons].each do |m|
-        message_button = MessageButton.new(m)
-        message_button.message_id = message.id
-        message_button.save
+        message.message_buttons << MessageButton.new(m)
       end
-
       mentions_attributes[:mentions].each do |m|
-        mention = Mention.new(m)
-        mention.message_id = message.id
-        mention.save
+        message.mentions << Mention.new(m)
       end
+      message.save!
     end
 
     # TODO: Send Message Button to mention users by Slack DM.
