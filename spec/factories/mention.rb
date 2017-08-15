7446d7523268b9ee5b5258cc67ca4596c725f6b6
takumakume <takuma.kume@gmail.com>
2017-08-15 16:18:40 +0900

    mentionのslack_idを修正

--- spec/factories/mention.rb
+++ spec/factories/mention.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :mention do
     message_id 1
-    slack_id 'Slack User 001'
   end
 end
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -31,6 +31,8 @@ RSpec.describe "Messages", type: :request do
       create(:user, id: 1)
       @message = create(:message, id: 1)
       create(:mention, id: 1)
+      create(:mention, id: 1, slack_id: "ABCDEFG01")
+      create(:mention, id: 2, slack_id: "ABCDEFG02")
       create(:message_button, id: 1, name: "name1", text: "label2")
       create(:message_button, id: 2, name: "name2", text: "label2")
 
