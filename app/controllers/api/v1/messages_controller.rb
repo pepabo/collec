f3c9fa8e787eab25c0ba68bf545838b623dfef04
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-15 15:35:21 +0900

    コメントを追記

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::MessagesController < ApplicationController
 
     Message.transaction do
       message = Message.new(message_attributes)
-      message.user_id = 1
+      message.user_id = 1 # TODO: Pass the user id parameter from payload user id in JWT.
       message.save
 
       message_buttons_attributes[:message_buttons].each do |m|
@@ -25,6 +25,8 @@ class Api::V1::MessagesController < ApplicationController
         mention.save
       end
     end
+
+    # TODO: Send Message Button to mention users by Slack DM.
 
     head :created
   end
