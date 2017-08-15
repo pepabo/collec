b706e6c4f6b81bf3bd609017043638d1171e85ef
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-15 17:10:47 +0900

    トランザクション部の調整
    * トランザクションをするのはsave処理のみにする
    * 例外が発生したらエラーログに出力する

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,14 +8,18 @@ class Api::V1::MessagesController < ApplicationController
     message_buttons_params = params.permit(message_buttons: [:text])
     mentions_params = params.permit(mentions: [:slack_id, :name, :profile_picture_url])
 
+    message = Message.new(message_params)
+    message.user_id = 1 # TODO: Pass the user id parameter from payload user id in JWT.
+
+    message.message_buttons = message_buttons_params[:message_buttons].map {|m| MessageButton.new(m) }
+    message.mentions = mentions_params[:mentions].map {|m| Mention.new(m) }
+
     Message.transaction do
-      message = Message.new(message_params)
-      message.user_id = 1 # TODO: Pass the user id parameter from payload user id in JWT.
-
-      message.message_buttons = message_buttons_params[:message_buttons].map {|m| MessageButton.new(m) }
-      message.mentions = mentions_params[:mentions].map {|m| Mention.new(m) }
-
-      message.save!
+      begin
+        message.save!
+      rescue e
+        Rails.logger.error e.inspect
+      end
     end
 
     # TODO: Send Message Button to mention users by Slack DM.
