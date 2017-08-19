04055e1b6b89a567d710362007576dc30e1f11ea
pyama86 <pyama@pepabo.com>
2017-08-19 12:58:31 +0900

    いらないテストを消す

--- test/controllers/messages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_message_url
-    assert_response :success
-  end
-
-end
