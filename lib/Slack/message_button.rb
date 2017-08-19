e89c05f5680d1ee10b2af22a14cc9997d7717704
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-19 18:04:07 +0900

    Slackのメッセージボタン作成ライブラリをざっと書いた

---
+++ lib/Slack/message_button.rb
@@ -0,0 +1,42 @@
+require 'securerandom'
+
+module Slack
+  class MessageButton
+    def client
+      @client ||= Slack::Web::Client.new
+    end
+
+    def create_options(options = {})
+      actions = create_actions(options[:message_buttons])
+
+      {
+        channel: options[:channel],
+        text: options[:text],
+        attachments: [
+          {
+            fallback: 'fallback', # TODO: Set fallback
+            callback_id: 'test', # TODO: Set callback id
+            color: '#3AA3E3',
+            attachment_type: 'default',
+            actions: actions
+          }
+        ]
+      }
+    end
+
+    def create_actions(message_buttons = [])
+      message_buttons.map do |message_button|
+        {
+          name: SecureRandom.hex(16),
+          text: message_button[:text],
+          type: 'button',
+        }
+      end
+    end
+
+    def direct_message(options = {})
+      options = create_options(options)
+      client.chat_postMessage(options)
+    end
+  end
+end
