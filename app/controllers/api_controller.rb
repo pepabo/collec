23fa4148b8cecc647f7a6305c4b7b9d335cb5508
pyama86 <pyama@pepabo.com>
2017-08-19 12:14:06 +0900

    fix rubocop

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,6 @@ class ApiController < ActionController::Base
 
   def authenticate_user!
     return if Rails.env.test?
-    render :text => 'Unauthorized', :status => 401 unless user_signed_in?
+    render text: 'Unauthorized', status: 401 unless user_signed_in?
   end
 end
