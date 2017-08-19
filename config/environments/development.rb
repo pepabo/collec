25b104ad5ccf662cc5cf3c40f8ecf49feac0c936
pyama86 <pyama@pepabo.com>
2017-08-19 13:02:50 +0900

    redisストア変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.session_store :redis_store, servers: 'redis://localhost:6379/cache', expire_in: 5.minutes
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -23,7 +22,6 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 3.minutes }
     config.cache_store = :null_store if ENV['RAILS_CACHE_IGNORE']
   end
 
