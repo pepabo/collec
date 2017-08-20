2bec9fa2472762646d23199248a3f39a13fbb84b
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-20 11:15:57 +0900

    テストケースを追加

---
+++ spec/lib/slack/message_button_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe Slack::MessageButton do
+  let(:slack) { Slack::MessageButton.new }
+
+  describe '#create_action' do
+    context 'when has no values' do
+      before do
+        @result = slack.send(:create_actions)
+      end
+
+      it { expect(@result.count).to eq 0 }
+      it { expect(@result).to eq [] }
+    end
+
+    context 'when has message button values' do
+      before do
+        @result = slack.send(:create_actions, [
+            { text: 'hoge' },
+            { text: 'fuga' },
+          ]
+        )
+      end
+
+      it { expect(@result.count).to eq 2 }
+      it { expect(@result[0][:name]).not_to be_empty }
+      it { expect(@result[0][:text]).to eq 'hoge' }
+      it { expect(@result[0][:type]).to eq 'button' }
+      it { expect(@result[1][:name]).not_to be_empty }
+      it { expect(@result[1][:text]).to eq 'fuga' }
+      it { expect(@result[1][:type]).to eq 'button' }
+    end
+  end
+end
