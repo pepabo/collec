fa740bc9f13e30f8e77daf64d61cc90ac554aaa3
Shinya Tsunematsu <s@tnmt.info>
2017-08-23 14:17:09 +0900

    mapを使って書き直し

--- lib/slack/user.rb
+++ lib/slack/user.rb
@@ -3,12 +3,13 @@ module Slack
     def list
       users_list = client.users_list['members'].select { |u| u['deleted'] == false && u['is_bot'] == false }
       users = []
-      users_list.each do |u|
-        user = {}
-        user[:slack_id] = u['id']
-        user[:name] = u['name']
-        user[:real_name] = u['real_name']
-        user[:avatar_url] = u['profile']['image_24']
+      users_list.map do |u|
+        user = {
+          slack_id: u['id'],
+          name: u['name'],
+          real_name: u['real_name'],
+          avatar_url: u['profile']['image_24']
+        }
         users.push user
       end
       users
