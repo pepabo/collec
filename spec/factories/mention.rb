2afec19e14860c26b3b164a50316f726552b9e7e
Shinya Tsunematsu <s@tnmt.info>
2017-08-23 22:51:15 +0900

    master と conflict するのでいったん削除

--- spec/factories/mention.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :mention do
-    message_id  1
-    slack_id    "U023BECGF"
-    name        "Slack User 001"
-    profile_picture_url "http://hoge.io/001.jpg"
-  end
-end
--- spec/factories/message_button.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :message_button do
-    message_id 1
-    name       "text"
-    text       "button001"
-  end
-end
