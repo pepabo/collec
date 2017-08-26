6f3420e2e47714163e70c2eac78234317c0cf600
takumakume <takuma.kume@gmail.com>
2017-08-26 21:16:21 +0900

    全体的に修正した

--- app/controllers/api/v1/message_answers_controller.rb
+++ app/controllers/api/v1/message_answers_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::MessageAnswersController < ApplicationController
   def create
     message_answers_params = params.permit(:message_id, :mention_id, :message_button_id)
-    message = Message.find(message_answers_params[:message_id])
 
-    unless message
-      raise "message not found"
-    end
+    #message = Message.find(message_answers_params[:message_id])
 
+    message_answer = MessageAnswer.new(message_answers_params)
     # TODO: https://github.com/honeymoon-answer/answer/pull/52
-    button_type = 'single'
-
-    my_answers = message.message_answers.find do |answer|
-      answer[:mention_id] == message_answers_params[:mention_id]
-    end
-
-    if my_answers
-      case button_type
-      when "single"
-        message_answers_params[:id] = my_answers.first[:id]
-      when "multi"
-        if my_answers.find { |a| a[:message_button_id] == message_answers_params[:message_button_id] }
-          message_answers_params[:id] = a[:id]
-        end
-      else
-        raise "unknown button_type"
-      end
+    #case message[:button_type]
+    answers = nil
+    case 'single'
+    when "single"
+      answers = MessageAnswer.where(
+        message_id: message_answers_params[:message_id],
+        mention_id: message_answers_params[:mention_id],
+      )
+    when "multi"
+      answers = MessageAnswer.where(
+        message_id: message_answers_params[:message_id],
+        mention_id: message_answers_params[:mention_id],
+        message_button_id: message_answers_params[:message_button_id]
+      )
     end
 
     MessageAnswer.transaction do
       begin
-        MessageAnswer.new(message_answers_params).save!
+        if answers.empty?
+          message_answer.save!
+        else
+          message_answer.update!
+        end
+        p MessageAnswer.all.first
       rescue => e
         Rails.logger.error e.inspect
       end
