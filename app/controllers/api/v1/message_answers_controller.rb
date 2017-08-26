66365c9cd9bd00c370d3d8eabee3b9ca75d848ff
takumakume <takuma.kume@gmail.com>
2017-08-26 16:33:59 +0900

    button_typeに応じたcallbackの動作を実装した

--- app/controllers/api/v1/message_answers_controller.rb
+++ app/controllers/api/v1/message_answers_controller.rb
@@ -1,14 +1,44 @@
 class Api::V1::MessageAnswersController < ApplicationController
   def create
     message_answers_params = params.permit(:message_id, :mention_id, :message_button_id)
+    message = Message.find(message_answers_params[:message_id])
 
+    unless message
+      raise "message not found"
+    end
+
+    # TODO: https://github.com/honeymoon-answer/answer/pull/52
+    message[:button_type] = 'single'
+
+    my_answers = message.message_answers.find do |answer|
+      answer[:mention_id] == message_answers_params[:mention_id]
+    end
+
+    if my_answers
+      case message[:button_type]
+      when "single"
+        message_answers_params[:id] = my_answers.first[:id]
+      when "multi"
+        if my_answers.find { |a| a[:message_button_id] == message_answers_params[:message_button_id] }
+          message_answers_params[:id] = a[:id]
+        end
+      else
+        raise "unknown button_type"
+      end
+    end
+
+    message_save!(message_answers_params)
+    head :created
+  end
+
+  private
+  def message_save!(params)
     MessageAnswer.transaction do
       begin
-        MessageAnswer.new(message_answers_params).save!
+        MessageAnswer.new(params).save!
       rescue => e
         Rails.logger.error e.inspect
       end
     end
-    head :created
   end
 end
