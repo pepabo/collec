68ee42cd7384704c1e4b77b2f54033cb466c179b
takumakume <takuma.kume@gmail.com>
2017-08-26 21:59:42 +0900

    singleの場合のcreate/updateは実装できた

--- app/controllers/api/v1/message_answers_controller.rb
+++ app/controllers/api/v1/message_answers_controller.rb
@@ -2,37 +2,15 @@ class Api::V1::MessageAnswersController < ApplicationController
   def create
     message_answers_params = params.permit(:message_id, :mention_id, :message_button_id)
 
-    #message = Message.find(message_answers_params[:message_id])
+    answer = MessageAnswer.where(
+      message_id: message_answers_params[:message_id],
+      mention_id: message_answers_params[:mention_id],
+    ).first
 
-    message_answer = MessageAnswer.new(message_answers_params)
-    # TODO: https://github.com/honeymoon-answer/answer/pull/52
-    #case message[:button_type]
-    answers = nil
-    case 'single'
-    when "single"
-      answers = MessageAnswer.where(
-        message_id: message_answers_params[:message_id],
-        mention_id: message_answers_params[:mention_id],
-      )
-    when "multi"
-      answers = MessageAnswer.where(
-        message_id: message_answers_params[:message_id],
-        mention_id: message_answers_params[:mention_id],
-        message_button_id: message_answers_params[:message_button_id]
-      )
-    end
-
-    MessageAnswer.transaction do
-      begin
-        if answers.empty?
-          message_answer.save!
-        else
-          message_answer.update!
-        end
-        p MessageAnswer.all.first
-      rescue => e
-        Rails.logger.error e.inspect
-      end
+    if answer
+      MessageAnswer.find(answer[:id]).update_attributes!(message_button_id: message_answers_params[:message_button_id])
+    else
+      MessageAnswer.new(message_answers_params).save!
     end
 
     head :created
--- spec/requests/message_answers_spec.rb
+++ spec/requests/message_answers_spec.rb
@@ -26,4 +26,37 @@ RSpec.describe "MessageAnswers", type: :request do
       expect(@message_answer[:message_button_id]).to eq @user_with_messages.messages.first.message_buttons.first.id
     end
   end
+
+#  describe "POST /api/v1/slack/interactive-messages/callback button_type single update message answer" do
+#    before do
+#      @user_with_messages = create(:user, :with_messages, id: 1)
+#
+#      create(
+#        :message_answer,
+#        message_id: @user_with_messages.messages.first.id,
+#        mention_id: @user_with_messages.messages.first.mentions.first.id,
+#        message_button_id: @user_with_messages.messages.first.message_buttons.first.id + 1,
+#      )
+#
+#      post "/api/v1/slack/interactive-messages/callback", params:
+#        {
+#          message_id: @user_with_messages.messages.first.id,
+#          mention_id: @user_with_messages.messages.first.mentions.first.id,
+#          message_button_id: @user_with_messages.messages.first.message_buttons.first.id,
+#        }
+#
+#      @message_answer = MessageAnswer.first
+#    end
+#
+#    it 'response 201' do
+#      expect(response).to be_success
+#      expect(response.status).to eq 201
+#    end
+#
+#    it 'check db registration' do
+#      expect(@message_answer[:message_id]).to eq @user_with_messages.messages.first.id
+#      expect(@message_answer[:mention_id]).to eq @user_with_messages.messages.first.mentions.first.id
+#      expect(@message_answer[:message_button_id]).to eq @user_with_messages.messages.first.message_buttons.first.id
+#    end
+#  end
 end
