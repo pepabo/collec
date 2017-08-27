0258e26b35f7afbb77289a4885e2b7350621fd2a
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-27 12:40:58 +0900

    仮の値でPOST送信できるようにする

--- app/javascript/packs/messages/new.js
+++ app/javascript/packs/messages/new.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Api from '../../lib/api'
+import $ from 'jquery'
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
@@ -14,6 +16,22 @@ document.addEventListener('DOMContentLoaded', () => {
       },
       removeMessageButton(key) {
         Vue.delete(this.messageButtons, key)
+      },
+      register() {
+        Api.Message.create(
+          $("input[name='authenticity_token'").val(),
+          {
+            message: 'hoge',
+            require_confirm: 0,
+            due_at: '2017-08-28 10:00:00'
+          },
+          [
+            { text: 'button_name001'}
+          ],
+          [
+            { slack_id: 'UHOGEHOGE', name: 'hypermkt', profile_picture_url: 'http://hogehoge.jp' }
+          ]
+        )
       }
     }
   })
