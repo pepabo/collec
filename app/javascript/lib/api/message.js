57309049f45b311c950c4b6a2d720c2a967773be
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-08-27 11:56:30 +0900

    APIクライアントにPOST /api/v1/messagesをリクエストするメソッドを追加

--- app/javascript/lib/api/message.js
+++ app/javascript/lib/api/message.js
@@ -3,5 +3,12 @@ import axios from 'axios'
 export default {
   list: () => {
     return axios.get('/api/v1/messages');
+  },
+  create: (message, message_buttons, mentions) => {
+    return axios.post('/api/v1/messages', { params: {
+      message,
+      message_buttons: message_buttons,
+      mentions: mentions
+    }});
   }
 }
