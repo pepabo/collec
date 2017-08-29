92348ef842965e7223c9e0f0a0ad543e448e62bf
Shinya Tsunematsu <s@tnmt.info>
2017-08-29 20:38:15 +0900

    数値のオーバーライド出来ないかね

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq', size: 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq', size: 1 }
 end
