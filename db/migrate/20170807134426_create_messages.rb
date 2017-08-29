3b3aca49298695dc0af3f0800ba937cd4f9ee518
Shinya Tsunematsu <s@tnmt.info>
2017-08-29 17:58:38 +0900

    postgresql は db:migrate の段階でエラーになるので過去の定義から直す

--- db/migrate/20170807134426_create_messages.rb
+++ db/migrate/20170807134426_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.references :user, foreign_key: true
       t.text :message, comment: 'Request message'
       t.datetime :dut_at, comment: 'Due to answer the request'
-      t.boolean :require_confirm, default: 0, comment: 'Flag to use confirmation dialog'
+      t.boolean :require_confirm, default: false, comment: 'Flag to use confirmation dialog'
 
       t.timestamps
     end
