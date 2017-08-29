c376b632b418690a9ceb64099ac81857520c9776
Shinya Tsunematsu <s@tnmt.info>
2017-08-29 18:39:47 +0900

    herokuのREDIS_URLは認証付きで複雑なのでスキーマ込みで環境変数に渡す
    またredis-rbで無指定の場合の接続先も同環境変数を参照している

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{Settings.redis.host}:6379", namespace: 'sidekiq' }
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{Settings.redis.host}:6379", namespace: 'sidekiq' }
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
 end
