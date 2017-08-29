010ff6fa3683b012c30c13397e1ac03f6e537c1a
takumakume <takuma.kume@pepabo.com>
2017-08-29 18:39:09 +0900

    single buttonのupdateのテストを修正

--- spec/requests/slack/interactive_messages_spec.rb
+++ spec/requests/slack/interactive_messages_spec.rb
@@ -39,16 +39,29 @@ RSpec.describe "InteractiveMessages", type: :request do
   describe "POST /api/v1/slack/interactive-messages/callback button_type single update message answer" do
     let!(:user) { create(:user) }
     let!(:message) { create(:message, user: user) }
-    let!(:button) { create(:message_button, message: message) }
+    let!(:buttons) { create_list(:message_button, 2, message: message) }
     let!(:mention) { create(:mention, message: message) }
 
     before do
-      # POST parameter: https://api.slack.com/docs/message-buttons
+      # create
       post "/api/v1/slack/interactive-messages/callback", params:
         {
           actions: [
-            name: button.name,
-            value: button.text,
+            name: buttons[1].name,
+            value: buttons[1].text,
+            type: 'button',
+          ],
+          user: {
+            id: mention.slack_id,
+          },
+        }
+
+      # update
+      post "/api/v1/slack/interactive-messages/callback", params:
+        {
+          actions: [
+            name: buttons.first.name,
+            value: buttons.first.text,
             type: 'button',
           ],
           user: {
@@ -67,7 +80,7 @@ RSpec.describe "InteractiveMessages", type: :request do
     it 'check db registration' do
       expect(@message_answer[:message_id]).to eq message.id
       expect(@message_answer[:mention_id]).to eq mention.id
-      expect(@message_answer[:message_button_id]).to eq button.id
+      expect(@message_answer[:message_button_id]).to eq buttons.first.id
     end
   end
 end
