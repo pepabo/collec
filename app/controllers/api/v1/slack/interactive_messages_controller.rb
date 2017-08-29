d5a8f6d54c8fdc358a8d1dd5a040ce1040d9a9e3
takumakume <takuma.kume@pepabo.com>
2017-08-29 17:40:50 +0900

    controllerの処理を一部modelに移動

--- app/controllers/api/v1/slack/interactive_messages_controller.rb
+++ app/controllers/api/v1/slack/interactive_messages_controller.rb
@@ -12,51 +12,11 @@ class Api::V1::Slack::InteractiveMessagesController < ApplicationController
 
     case message[:button_type]
     when "single"
-      create_of_single_button(message_answers_params)
+      MessageAnswer.create_of_single_button(message_answers_params)
     when "multi"
-      create_of_multi_button(message_answers_params)
+      MessageAnswer.create_of_multi_button(message_answers_params)
     end
 
     head :created
-  end
-
-  private
-  def create_of_single_button(message_answers_params)
-    answer = MessageAnswer.find_by(
-      message_id: message_answers_params[:message_id],
-      mention_id: message_answers_params[:mention_id],
-    )
-
-    MessageAnswer.transaction do
-      begin
-        if answer
-          MessageAnswer.find(answer[:id]).update_attributes!(message_button_id: message_answers_params[:message_button_id])
-        else
-          MessageAnswer.new(message_answers_params).save!
-        end
-      rescue => e
-        Rails.logger.error e.inspect
-      end
-    end
-  end
-
-  def create_of_multi_button(message_answers_params)
-    answer = MessageAnswer.find_by(
-      message_id: message_answers_params[:message_id],
-      mention_id: message_answers_params[:mention_id],
-      message_button_id: message_answers_params[:message_button_id],
-    )
-
-    MessageAnswer.transaction do
-      begin
-        if answer
-          MessageAnswer.delete(answer[:id])
-        else
-          MessageAnswer.new(message_answers_params).save!
-        end
-      rescue => e
-        Rails.logger.error e.inspect
-      end
-    end
   end
 end
--- app/models/message_answer.rb
+++ app/models/message_answer.rb
@@ -2,4 +2,43 @@ class MessageAnswer < ApplicationRecord
   belongs_to :message
   belongs_to :mention
   belongs_to :message_button
+
+  def self.create_of_single_button(message_answers_params)
+    answer = MessageAnswer.find_by(
+      message_id: message_answers_params[:message_id],
+      mention_id: message_answers_params[:mention_id],
+    )
+
+    MessageAnswer.transaction do
+      begin
+        if answer
+          MessageAnswer.find(answer[:id]).update_attributes!(message_button_id: message_answers_params[:message_button_id])
+        else
+          MessageAnswer.new(message_answers_params).save!
+        end
+      rescue => e
+        Rails.logger.error e.inspect
+      end
+    end
+  end
+
+  def self.create_of_multi_button(message_answers_params)
+    answer = MessageAnswer.find_by(
+      message_id: message_answers_params[:message_id],
+      mention_id: message_answers_params[:mention_id],
+      message_button_id: message_answers_params[:message_button_id],
+    )
+
+    MessageAnswer.transaction do
+      begin
+        if answer
+          MessageAnswer.delete(answer[:id])
+        else
+          MessageAnswer.new(message_answers_params).save!
+        end
+      rescue => e
+        Rails.logger.error e.inspect
+      end
+    end
+  end
 end
