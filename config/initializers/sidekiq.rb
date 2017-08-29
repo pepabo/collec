6a68e950ef4bdb9c12e21b8cb41d581faa7aba45
Shinya Tsunematsu <s@tnmt.info>
2017-08-29 20:44:52 +0900

    Revert "数値のオーバーライド出来ないかね"
    値小さすぎても実行出来ないぽ
    
    This reverts commit 92348ef842965e7223c9e0f0a0ad543e448e62bf.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq', size: 1 }
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq', size: 1 }
+  config.redis = { url: ENV['REDIS_URL'], namespace: 'sidekiq' }
 end
