a32c9293280c83f70cdc18ed59349c52979d6038
pyama86 <pyama@pepabo.com>
2017-08-30 16:21:57 +0900

    pagenate

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.all.page(params[:page]).per(10)
   end
 
   def show
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @messages = Message.all.page(params[:page]).per(10)
   end
 
   def new
