5b556cdd84f57022396a50c52f7590f0b104e1b7
pyama86 <pyama@pepabo.com>
2017-09-13 15:00:41 +0900

    リマインドの対象は翌日以降

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -52,7 +52,7 @@ class Api::V1::MessagesController < ApplicationController
       remind_day = message.due_at - 1.day
       wait = Time.new(remind_day.year, remind_day.month, remind_day.day, REMIND_HOUR, REMIND_MINUTE) - Time.now
       RemindWorker.perform_at(wait, m.id)
-    end
+    end if Date.today + 2 < message_due_at
 
     render json: message.to_json(include: %w(message_buttons mentions) ), status: :created
   end
