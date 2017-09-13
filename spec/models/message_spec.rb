1f800228ef3d2733a49928ff275ff660aa3b45e9
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-09-14 01:29:24 +0900

    コメント調整

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe Message, type: :model do
   describe '#calc_percentage' do
-    context 'Divisible' do
+    context 'When giving divisible values' do
       before { @result = Message.calc_percentage(1, 2) }
       it { expect(@result).to eq 50 }
     end
 
-    context 'Not divisible' do
+    context 'When giving no divisible values' do
       before { @result = Message.calc_percentage(4, 11) }
       it { expect(@result).to eq 36 }
     end
