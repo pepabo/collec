ca048faa8109f8dfd392d9d09bf803bc9cbf03dd
Makoto Chiba <hypermkt@paperboy.co.jp>
2017-09-19 19:42:48 +0900

    回答0件時にゼロ除算が発生し、アンケート詳細画面が閲覧できなくなっていた

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,8 @@ class Message < ApplicationRecord
   enum button_type: %w(single multi)
 
   def self.calc_percentage(num, total)
+    return 0 if total == 0
+
     # No decimal point required. ex) 36.36363636363637 to 36
     ((num / total.to_f) * 100).round
   end
